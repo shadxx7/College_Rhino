ccb37eafcdea3d5e697f2dd239831575a37ffe02
Mohamed Shadab <shadabdragon@gmail.com>
2017-09-09 04:29:34 +0530

    Them Changes

--- MyApplication/app/src/main/java/com/example/android/educonnect/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/android/educonnect/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.educonnect;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,6 +12,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
