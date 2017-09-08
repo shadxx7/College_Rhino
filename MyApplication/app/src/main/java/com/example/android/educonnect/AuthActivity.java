567e09983780d5eeeddf2bb5a45de0707606f778
Mohamed Shadab <shadabdragon@gmail.com>
2017-09-09 04:41:00 +0530

    Them Changes

--- MyApplication/app/src/main/java/com/example/android/educonnect/AuthActivity.java
+++ MyApplication/app/src/main/java/com/example/android/educonnect/AuthActivity.java
@@ -2,16 +2,22 @@ package com.example.android.educonnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class AuthActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        mToolbar = (Toolbar) findViewById(R.id.auth_toolbar);
+        setSupportActionBar(mToolbar);
     }
 
 
--- MyApplication/app/src/main/java/com/example/android/educonnect/RegisterActivity.java
+++ MyApplication/app/src/main/java/com/example/android/educonnect/RegisterActivity.java
@@ -2,12 +2,19 @@ package com.example.android.educonnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mToolbar = (Toolbar) findViewById(R.id.register_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Register");
     }
 }
