0c8dcd53c3c61c59c2f84765355c868fcd9a395b
bravoo84 <bhavikmehta98@gmail.com>
2017-09-10 01:00:06 +0530

    Youtube linking

--- MyApplication/app/src/main/java/com/example/android/educonnect/videoFragment.java
+++ MyApplication/app/src/main/java/com/example/android/educonnect/videoFragment.java
@@ -9,27 +9,35 @@ package com.example.android.educonnect;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
+        import android.widget.EditText;
+        import android.widget.TextView;
         import android.widget.Toast;
         import com.google.android.youtube.player.YouTubeInitializationResult;
         import com.google.android.youtube.player.YouTubePlayer;
         import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
         import com.google.android.youtube.player.YouTubePlayer.Provider;
         import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+        import com.google.firebase.database.DataSnapshot;
+        import com.google.firebase.database.DatabaseError;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.database.ValueEventListener;
 
 
 public class videoFragment extends Fragment {
     private static final String API_KEY = "AIzaSyDOpiT-vlu-SveEYggS0LbQaOzVhRqF8Y8";
-
-
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private TextView mLinkTv;
     private static String VIDEO_ID = "EGy39OMyHzw";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.videos1, container, false);
 
-
+        firebaseDatabase=FirebaseDatabase.getInstance();
         YouTubePlayerSupportFragment youTubePlayerFragment = YouTubePlayerSupportFragment.newInstance();
-
+        mLinkTv=(TextView)rootView.findViewById(R.id.link1);
 
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         transaction.add(R.id.youtube_layout, youTubePlayerFragment).commit();
@@ -54,6 +62,20 @@ public class videoFragment extends Fragment {
             }
         });
 
+        databaseReference=firebaseDatabase.getReference("YoutubeLinks").child("link1");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String get_link=(String)dataSnapshot.getValue();
+                mLinkTv.setText(get_link);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
         return rootView;
+
     }
 }
