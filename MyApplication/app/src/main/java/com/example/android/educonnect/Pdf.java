06012c980f2140f0429c2a60552e8585551d0d1f
bravoo84 <bhavikmehta98@gmail.com>
2017-09-09 17:43:58 +0530

    pdf firebase

---
+++ MyApplication/app/src/main/java/com/example/android/educonnect/Pdf.java
@@ -0,0 +1,76 @@
+package com.example.android.educonnect;
+
+import android.app.LauncherActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.URL;
+
+public class Pdf extends AppCompatActivity {
+    PDFView pdfView;
+    String[] pdflist;
+    File[] imagelist;
+    private static int PICK_GALLERY_PDF=1;
+    private Button mBtn;
+    private StorageReference mStorageRef;
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+        pdfView=(PDFView) findViewById(R.id.pdfView);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        mProgressDialog=new ProgressDialog(this);
+                Intent intentGalley = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intentGalley.addCategory(Intent.CATEGORY_OPENABLE);
+                intentGalley.setType("application/pdf");
+                //startActivity(intentGalley);
+                startActivityForResult(intentGalley, PICK_GALLERY_PDF);
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //mProgressDialog.setTitle("Uploading pdf to server.Please wait.");
+        mProgressDialog.show();
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        Uri pdf=data.getData();
+        pdfView.fromUri(pdf).load();
+        mStorageRef.putFile(pdf).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                mProgressDialog.dismiss();
+                //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Pdf.this,"Couldnt upload!",Toast.LENGTH_SHORT).show();
+                mProgressDialog.dismiss();
+            }
+        });
+    }
+}
